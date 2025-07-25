fe993a4ec8e36cf2d2fa69a53748a0e49466a0b0
Dan Wood <207080+danwood@users.noreply.github.com>
2025-07-25 09:17:01 -0700

    Initial commit adding redundant internal and redundant fileprivate access

--- Sources/SourceGraph/Elements/Declaration.swift
+++ Sources/SourceGraph/Elements/Declaration.swift
@@ -235,6 +235,7 @@ public final class Declaration {
     public var related: Set<Reference> = []
     public var isImplicit: Bool = false
     public var isObjcAccessible: Bool = false
+    public var referencedFiles: Set<SourceFile>
 
     private let hashValueCache: Int
 
@@ -300,6 +301,7 @@ public final class Declaration {
         self.kind = kind
         self.usrs = usrs
         self.location = location
+        self.referencedFiles = [location.file]
         hashValueCache = usrs.hashValue
     }
 
